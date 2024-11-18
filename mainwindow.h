9fbb2012cb8b43f4f1c818e091f2f6b33b172121
Javier Armas <71993977+javierarmas@users.noreply.github.com>
2024-11-17 18:06:00 -0600

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -31,6 +31,12 @@ private slots:
     // Genera el archivo de resultados al presionar el boton "Obtener Resultados"
     void obtenerResultados();
 
+    // Interrumpe los procesos que estan en ejecucion
+    void interrumpirProceso();
+
+    // Termina los procesos que estan en ejecucion y los manda a "Terminados"
+    void terminarConError();
+
 private:
     Ui::MainWindow *ui;  // Interfaz grafica
     QTimer *relojGlobal;  // Control del reloj global
@@ -40,20 +46,26 @@ private:
     int procesosPendientes;  // Cantidad de procesos pendientes
     int lotesPendientes;  // Numero de lotes pendientes
     int numeroProcesos;  // Total de procesos ingresados por el usuario
+    int calcularResultado(QString operacion); // Calcula el resultado de una operacion asignada a un proceso
 
     std::vector<QString> nombres;  // Almacena los nombres de los procesos
     std::vector<QString> operaciones;  // Almacena las operaciones matematicas de los procesos
     std::vector<int> TMEs;  // Tiempos maximos estimados (TME) para cada proceso
+    std::vector<int> TEs;  // Vector para almacenar el Tiempo Ejecutado de cada proceso
     std::vector<int> TMEOriginales;  // Almacena los TME originales para cada proceso
+    std::vector<QString> estados;  // Vector para almacenar el estado de cada proceso ("PENDIENTE", "FINALIZADO", "ERROR")
+    std::vector<int> ids; // Mantener los numeros originales de cada proceso
 
-    // Genera nombres aleatorios para los procesos
-    QString generarNombre();
+    std::vector<int> idsOriginales; // Almacena los ids originales para cada proceso
+    std::vector<QString> nombresOriginales; // Almacena los nombres originales para cada proceso
+    std::vector<QString> operacionesOriginales; // Almacena las operaciones originales para cada proceso
+    std::vector<QString> estadosOriginales; // Almacena los estados originales para cada proceso
+    std::vector<bool> interrumpidos;  // Vector para indicar si un proceso ha sido interrumpido
 
-    // Genera una operacion matematica aleatoria para los procesos
-    QString generarOperacion();
+    QString generarNombre(); // Genera nombres aleatorios para los procesos
+    QString generarOperacion(); // Genera una operacion matematica aleatoria para los procesos
 
-    // Calcula el resultado de una operacion asignada a un proceso
-    int calcularResultado(QString operacion);
+    QMap<int, int> tmeOriginalMap; // Mapa de ID de proceso a TME original
 };
 
 #endif // MAINWINDOW_H
